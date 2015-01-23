74129c3fa6e72b741d6270e58566ea4da5d3b9fe
Gustavo Carrillo <gustavocarrillo@nop.al>
2015-01-22 20:03:52 -0600

    Leccion de imagenes y filtros.

--- app.js
+++ app.js
@@ -6,13 +6,35 @@
     var app = angular.module('store', []);
 
     app.controller('StoreController', function(){
-        this.product = gem;
+        this.products = gems;
     });
 
-    var gem = {
-        name: 'Dodecahedron',
-        price: 2.95,
-        description: 'The description of the gem',
-        canPurchase: false
-    }
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'The description of the gem',
+            canPurchase: true,
+            soldOut: false,
+            images: [
+                {
+                    full: "images/img1.jpg",
+                    thumb: "tb_image1.jpg"
+                }
+            ]
+        },
+        {
+            name: 'Rune of powa!',
+            price: 13.37,
+            description: 'Pew pew pew!',
+            canPurchase: true,
+            soldOut: false,
+            images: [
+                {
+                    full: "img2.jpg",
+                    thumb: "tb_image2.jpg"
+                }
+            ]
+        }
+    ]
 })();
