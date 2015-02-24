85efc8289a7edd8f24861eea3508bdbbae5d8936
CesRmz <cesar.rare@gmail.com>
2015-02-23 21:28:42 -0600

    Fixed #1
    Login acepta credenciales y redirecciona a Index.html en caso de ser
    aprobados o muestra mensaje de error si falla la autenticación

---
+++ script/script.js
@@ -0,0 +1,29 @@
+
+   function llamarAPI()
+    {
+		debugger;
+        var usuario = $("#usuarioVal").val();
+        var password = $("#passwordVal").val();
+        $.ajax({
+            type: "POST",
+            url: "http://jingle.nop.al/API/usuarios/login",
+            data: "",
+            dataType: "json",
+            beforeSend: function (xhr){
+                xhr.setRequestHeader("Authorization", make_base_auth(usuario,password));
+            },
+            success: function (data)
+            {
+				window.location.href = "index.html"
+            },
+			error : function(data){
+				$('#errorMessage').html("Usuario o Contraseña Incorrectos");
+			}
+        });
+    }
+    function make_base_auth(user, password)
+    {
+        var tok = user + ':' + password;
+        var hash = btoa(tok);
+        return "Basic " + hash;
+    }
