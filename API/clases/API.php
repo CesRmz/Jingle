e2f500a79979a4063a8bee0dc6926c9a09c47c80
Gustavo Carrillo <gustavocarrillo@nop.al>
2015-02-23 20:42:03 -0600

    Habilité CORS

--- API/clases/API.php
+++ API/clases/API.php
@@ -41,9 +41,11 @@ abstract class API
      * Constructor: __construct
      * Allow for CORS, assemble and pre-process the data
      */
+
     public function __construct($request)
     {
-        header("Access-Control-Allow-Orgin: *");
+        $this->cors();
+        //header("Access-Control-Allow-Origin:*");
         header("Access-Control-Allow-Methods: *");
         header("Content-Type: application/json");
 
@@ -84,6 +86,28 @@ abstract class API
         }
     }
 
+    public function cors()
+    {
+        // Allow from any origin
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
+
+        // Access-Control headers are received during OPTIONS requests
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+            exit(0);
+        }
+    }
+
     /** ------------------ Segunda parte ------------------ */
 
     public function processAPI()
@@ -114,7 +138,6 @@ abstract class API
 
     private function _response($data, $status = 200)
     {
-        header('Content-Type: application/json');
         header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
         return $data;
     }
